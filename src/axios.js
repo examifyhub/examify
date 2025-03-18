114caee18808c104242b4178cd1cdad73fc734bf
Sanga_Bhattacharjee <sangabhattacharjee544@gmail.com>
2025-03-19 00:52:44 +0530

    Delete src directory

--- src/axios.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-import API_BASE_URL from './axiosConfig';
-
-axios.post(`${API_BASE_URL}/api/login`, { username, password });
--- src/axiosConfig.js
+++
@@ -1,5 +0,0 @@
-const API_BASE_URL = process.env.NODE_ENV === 'development' 
-  ? 'http://localhost:5000' 
-  : 'https://your-deployed-backend-url.com';
-
-export default API_BASE_URL;
