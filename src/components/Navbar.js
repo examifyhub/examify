5cfed78cb9514e3df0325de739aedf2051b75055
Sanga_Bhattacharjee <sangabhattacharjee544@gmail.com>
2025-03-20 10:41:44 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,60 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
-import logo from "../assets/examify-logo.png"; // ✅ Make sure this path is correct!
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   return (
-    <>
-      <nav className="navbar">
-        {/* ✅ Logo */}
-        <div className="navbar-logo">
-          <Link to="/">
-            <img src={logo} alt="Examify" className="logo-image" />
-          </Link>
-        </div>
-
-        {/* ✅ Mobile Menu Icon */}
-        <div className="menu-icon" onClick={toggleMenu}>
-          {menuOpen ? <FaTimes /> : <FaBars />}
-        </div>
-
-        {/* ✅ Navbar Links */}
-        <ul className={`navbar-links ${menuOpen ? "active" : ""}`}>
-          <li>
-            <Link to="/home" onClick={toggleMenu}>
-              <FaHome className="icon" /> Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/dashboard" onClick={toggleMenu}>
-              <FaTachometerAlt className="icon" /> Dashboard
-            </Link>
-          </li>
-          <li>
-            <Link to="/recommendation" onClick={toggleMenu}>
-              <FaBook className="icon" /> Recommendation
-            </Link>
-          </li>
-          <li>
-            <Link to="/conduct-exam" onClick={toggleMenu}>
-              <FaEdit className="icon" /> Conduct Exam
-            </Link>
-          </li>
-          <li>
-            <Link to="/logout" className="logout-button" onClick={toggleMenu}>
-              <FaSignOutAlt className="icon" /> Logout
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <nav className="navbar">
+      <div className="navbar-logo">Examify</div>
+      <div className="navbar-links">
+        <Link to="/home">Home</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/recommendation">Recommendation</Link>
+        <Link to="/conduct-exam">Conduct Exam</Link>
+        <Link to="/logout">Logout</Link>
+      </div>
+    </nav>
   );
 };
 
