a2361c171d2c31e801f78bb43ddd658af3bfcaa3
Sanga_Bhattacharjee <sangabhattacharjee544@gmail.com>
2025-03-20 09:43:44 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "../styles/Navbar.css";
+import { FaBars, FaTimes, FaHome, FaUser, FaSignInAlt, FaUserPlus } from "react-icons/fa";
+import "../styles/Navbar.css"; // Make sure you have this CSS file
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
-      <div className="navbar-logo">Examify</div>
-      <div className="navbar-links">
-        <Link to="/home">Home</Link>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/recommendation">Recommendation</Link>
-        <Link to="/conduct-exam">Conduct Exam</Link>
-        <Link to="/logout">Logout</Link>
+      <div className="navbar-container">
+        {/* ✅ Logo */}
+        <Link to="/" className="logo">
+          Examify
+        </Link>
+
+        {/* ✅ Mobile Menu Icon */}
+        <div className="menu-icon" onClick={toggleMenu}>
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </div>
+
+        {/* ✅ Navbar Links */}
+        <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+          <li>
+            <Link to="/" onClick={toggleMenu}>
+              <FaHome /> Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/profile" onClick={toggleMenu}>
+              <FaUser /> Profile
+            </Link>
+          </li>
+          <li>
+            <Link to="/login" onClick={toggleMenu}>
+              <FaSignInAlt /> Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/register" onClick={toggleMenu}>
+              <FaUserPlus /> Register
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
