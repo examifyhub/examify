3c07b73c3822fe2777f875cb89aab1d004ce3fa5
Sanga_Bhattacharjee <sangabhattacharjee544@gmail.com>
2025-03-20 09:53:01 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaBars, FaTimes, FaHome, FaUser, FaSignInAlt, FaUserPlus } from "react-icons/fa";
-import "../styles/Navbar.css"; // Make sure you have this CSS file
+import { FaBars, FaTimes, FaHome, FaTachometerAlt, FaBook, FaEdit, FaSignOutAlt } from "react-icons/fa";
+import "../styles/Navbar.css";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -12,41 +12,43 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <div className="navbar-container">
-        {/* ✅ Logo */}
-        <Link to="/" className="logo">
-          Examify
-        </Link>
-
-        {/* ✅ Mobile Menu Icon */}
-        <div className="menu-icon" onClick={toggleMenu}>
-          {menuOpen ? <FaTimes /> : <FaBars />}
-        </div>
-
-        {/* ✅ Navbar Links */}
-        <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
-          <li>
-            <Link to="/" onClick={toggleMenu}>
-              <FaHome /> Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/profile" onClick={toggleMenu}>
-              <FaUser /> Profile
-            </Link>
-          </li>
-          <li>
-            <Link to="/login" onClick={toggleMenu}>
-              <FaSignInAlt /> Login
-            </Link>
-          </li>
-          <li>
-            <Link to="/register" onClick={toggleMenu}>
-              <FaUserPlus /> Register
-            </Link>
-          </li>
-        </ul>
+      <div className="navbar-logo">
+        <Link to="/" style={{ color: "white", textDecoration: "none" }}>Examify</Link>
       </div>
+
+      {/* Mobile Menu Icon */}
+      <div className="menu-icon" onClick={toggleMenu}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      {/* Navbar Links */}
+      <ul className={`navbar-links ${menuOpen ? "active" : ""}`}>
+        <li>
+          <Link to="/home" onClick={toggleMenu}>
+            <FaHome /> Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/dashboard" onClick={toggleMenu}>
+            <FaTachometerAlt /> Dashboard
+          </Link>
+        </li>
+        <li>
+          <Link to="/recommendation" onClick={toggleMenu}>
+            <FaBook /> Recommendation
+          </Link>
+        </li>
+        <li>
+          <Link to="/conduct-exam" onClick={toggleMenu}>
+            <FaEdit /> Conduct Exam
+          </Link>
+        </li>
+        <li>
+          <Link to="/logout" onClick={toggleMenu}>
+            <FaSignOutAlt /> Logout
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
